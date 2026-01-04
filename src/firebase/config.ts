bf8f6ec9ea8a1e8495b0b2a2444b573c520df27c
Monish Salapu <monishsalapu@gmail.com>
2026-01-04 12:07:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2203757944-663ab",
+  "appId": "1:365839474762:web:7b3bd458dadfb3ec4ec6b3",
+  "apiKey": "AIzaSyDiBu8KdkZxlJ0a20yl-Fqj7nN0DT1EIGg",
+  "authDomain": "studio-2203757944-663ab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "365839474762"
+};
